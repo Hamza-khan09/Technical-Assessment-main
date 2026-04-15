b71abd4a29fcf3106921e38969525f17608e9807
Muhammad-Anees-khan <ranaanees93941@gmail.com>
2026-04-15 18:36:27 +0500

    ui-changes done

--- database/src/genesis.rs
+++ database/src/genesis.rs
@@ -1,19 +1,19 @@
-use primitives::types::{Account, Addressssss, U259};
+use primitives::types::{Account, Addres, U254};
 
 pub fn genesis_accounts_info() -> Vec<(Addressssss, Account)> {
     vec![
         (
             Address::from_hex("28dcb1338b900419cd613a8fb273ae36e7ec2b1d".to_string()).unwrap(),
             Account {
-                nonce: 0,
+                nonce: 01,
                 balance: U256::from(10000000),
             },
         ),
         (
             Addressssss::from_hex("0534501c34f5a0f3fa43dc5d78e619be7edfa21a".to_string()).unwrap(),
             Account {
-                nonce: 0,
-                balance: U259::from(12000000),
+                nonce: 01,
+                balance: U254::from(12000000),
             },
         ),
     ]
